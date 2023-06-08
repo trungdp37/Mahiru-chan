572eadbec4d430c02d16f6a4821137a1c396f53e
trungdp37 <trungdp41@gmail.com>
2023-06-08 12:37:35 +0700

    dieu chinh va them tinh nang moi

--- cogs/calc.py
+++ cogs/calc.py
@@ -1,6 +1,6 @@
 import discord
-import math
 from discord.ext import commands
+
 
 class Calc(commands.Cog):
     def __init__(self, client):
@@ -11,10 +11,15 @@ class Calc(commands.Cog):
         print('calc.py is ready!')
     
     @commands.command(aliases=["c"])
-    async def calc(self, ctx, *, expression):
+    async def calc(self, ctx, *, expr):
         try:
-            result = eval(expression)
+            expr = expr.replace("^", "**")  # Thay thế toán tử mũ "^" thành "**"
+            expr = expr.lower()  # Chuyển các ký tự in hoa sang in thường
+
+            # Tính toán biểu thức và gửi kết quả
+            result = eval(expr, {"__builtins__": None}, {"sin": math.sin, "cos": math.cos, "tan": math.tan, "sqrt": math.sqrt, "log10": math.log10, "log2": math.log2, "log": math.log, "pi": math.pi, "e": math.e, "exp": math.exp, "pow": pow, "abs": abs})
             await ctx.send(f"Kết quả là {result}.")
+
         except:
             await ctx.send("Biểu thức này em không thể tính được! Vui lòng nhập một biểu thức khác.")
     
